9caf742c4617a443c14dd381a552207fad85d8ab
Philippe Roussel <philippe.roussel@octets.fr>
2011-11-04 22:38:34 +0000

    Notation : copie des fichiers apache sur l'application de gestion createch qui fonctionne
    git-svn-id: svn+ssh://bipbip/home/svn/octets/cfai/apprentis/trunk@2232 22393631-9731-0410-a56f-bd5f96f33d13

--- wsgi_handler.py
+++ wsgi_handler.py
@@ -2,7 +2,7 @@ import os, sys
 
 sys.path.append('/home/www/cfai')
 sys.path.append('/home/www/cfai/notation')
-os.environ['DJANGO_SETTINGS_MODULE'] = 'notation.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cfai.settings'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
