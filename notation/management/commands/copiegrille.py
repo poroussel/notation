b71dc3b718c2844f6b54187240dbf292e9b0db25
Philippe Roussel <philippe.roussel@octets.fr>
2011-12-18 19:35:44 +0000

    Notation : ajout d'une commande admin pour copier une grille et ses éléments
    git-svn-id: svn+ssh://bipbip/home/svn/octets/cfai/apprentis/trunk@2430 22393631-9731-0410-a56f-bd5f96f33d13

---
+++ notation/management/commands/copiegrille.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+from copy import copy
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from django.conf import settings
+from cfai.notation.models import *
+
+class Command(BaseCommand):
+    help = u'Copie d\'une grille de notation et des objets associés'
+    args = "id_grille_origine promotion_nouvelle_grille"
+
+    def handle(self, *args, **options):
+        if len(args) < 2:
+            print u'Mauvais paramètres'
+            return
+        
+        id_grille = int(args[0])
+        promotion = int(args[1])
+
+        grille = GrilleNotation.objects.get(id=id_grille)        
+        ensembles = EnsembleCapacite.objects.filter(grille=grille)
+
+        ngrille = copy(grille)
+        ngrille.id = None
+        ngrille.promotion = promotion
+        ngrille.save()
+
+        for ens in ensembles:
+            capacites = Capacite.objects.filter(ensemble=ens)
+            nens = copy(ens)
+            nens.id = None
+            nens.grille = ngrille
+            nens.save()
+
+            for cap in capacites:
+                ncap = copy(cap)
+                ncap.id = None
+                ncap.ensemble = nens
+                ncap.save()
+                
+        print u'Copie de la grille %s pour l\'année %d' % (grille, promotion)
