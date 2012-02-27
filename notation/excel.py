033aaaf8888eb665cd6264f6b3fa9f2d51af9755
Philippe Roussel <philippe.roussel@octets.fr>
2012-02-27 10:33:50 +0000

    Notation : début de la mise en page du fichier excel généré
    git-svn-id: svn+ssh://bipbip/home/svn/octets/cfai/apprentis/trunk@2494 22393631-9731-0410-a56f-bd5f96f33d13

--- notation/excel.py
+++ notation/excel.py
@@ -16,16 +16,49 @@ def bulletin_xls(request, blt):
     book = Workbook(encoding='cp1251')
     sheet = book.add_sheet('Bulletin %d-%d' % (blt.grille.promotion, blt.grille.promotion + blt.grille.duree))
 
-    # Les entêtes de colonnes sur la deuxième ligne
-    sheet.write(2, 0, u'Partie spécifique')
-    sheet.write(2, 1, u'Capacités professionnelles et Tâches professionnelles (Etre capable de…)')
-    sheet.write(2, 2, u'1ère année')
-    sheet.write(2, 3, u'2ème année')
-    sheet.write(2, 4, u'3ème année')
-    sheet.write(2, 5, u'Cours associé')
-    sheet.write(2, 6, u'Résultats - indicateurs de performance - validation (Actions réalisées ou initiées en entreprise)')
+    # Entête globale
+    gras = easyxf('font: name Arial, color-index black, bold on; pattern: pattern solid, fore-colour light-green')
+    sheet.col(1).width = 15000
+    lig = 0
+    sheet.write_merge(lig, lig, 1, 4, u'Diplôme d\'ingénieur ENSMM Intitulé : ', gras)
+    lig += 1
+    sheet.write_merge(lig, lig, 1, 4, u'Filière ITII, Organisme coordinateur CFAI Sud Franche-Comté; Branche Professionnelle UIMM', gras)
+    lig += 1
 
-    lig = 3
+    gros = easyxf('font: name Arial, color-index black, bold on, height 240')
+    normal = easyxf('font: name Arial, color-index black, bold on, height 180')
+    sheet.write(lig, 1, u'Apprenti : ', gros)
+    sheet.row(lig).height = sheet.row(lig).height * 3 / 2
+    lig += 1
+    sheet.write(lig, 1, u'Année de formation : ', normal)
+    lig += 1
+    sheet.write(lig, 1, u'N° de tél portable : Adresse email : ', normal)
+    lig += 1
+    
+    sheet.write(lig, 1, u'Entreprise : ', gros)
+    sheet.row(lig).height = sheet.row(lig).height * 3 / 2
+    lig += 1
+    sheet.write(lig, 1, u'Adresse : ', normal)
+    lig += 1
+    
+    sheet.write(lig, 1, u'Tuteur : ', gros)
+    sheet.row(lig).height = sheet.row(lig).height * 3 / 2
+    lig += 1
+    sheet.write(lig, 1, u'N° de tél portable : Adresse email : ', normal)
+    lig += 1
+    sheet.write(lig, 1, u'Chargé de promotion : ', normal)
+    
+    lig = 13
+    # Les entêtes de colonnes sur la deuxième ligne
+    sheet.write(lig, 0, u'Partie spécifique')
+    sheet.write(lig, 1, u'Capacités professionnelles et Tâches professionnelles (Etre capable de…)')
+    sheet.write(lig, 2, u'1ère année')
+    sheet.write(lig, 3, u'2ème année')
+    sheet.write(lig, 4, u'3ème année')
+    sheet.write(lig, 5, u'Cours associé')
+    sheet.write(lig, 6, u'Résultats - indicateurs de performance - validation (Actions réalisées ou initiées en entreprise)')
+
+    lig += 1
     ensembles = EnsembleCapacite.objects.filter(grille = blt.grille)
     for ens in ensembles:
         capacites = Capacite.objects.filter(ensemble = ens).order_by('numero')
@@ -34,7 +67,7 @@ def bulletin_xls(request, blt):
         
         sheet.write(lig, 0, ens.partie)
         sheet.write(lig, 1, u'%d %s' % (ens.numero, ens.libelle))
-        lig = lig + 1
+        lig += 1
 
         for cap in capacites:
             sheet.write(lig, 1, u'%d.%d %s' % (ens.numero, cap.numero, cap.libelle))
