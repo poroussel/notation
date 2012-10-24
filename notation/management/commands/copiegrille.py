f3b43fb4bcab152a08822d861bd69b9b8fc6a9da
Philippe Roussel <philippe.roussel@octets.fr>
2012-10-24 18:51:40 +0000

    Notation : adaptation de la commande admin de copie de grille
    git-svn-id: svn+ssh://bipbip/home/svn/octets/cfai/apprentis/trunk@2721 22393631-9731-0410-a56f-bd5f96f33d13

--- notation/management/commands/copiegrille.py
+++ notation/management/commands/copiegrille.py
@@ -10,8 +10,8 @@ from cfai.notation.models import *
 # FIXME : modifier pour uniquement copier le contenu d'une grille existante
 # vers une autre grille existante (créée dans l'admin)
 class Command(BaseCommand):
-    help = u'Copie d\'une grille de notation et des objets associés'
-    args = "id_grille_origine promotion_nouvelle_grille"
+    help = u'Copie d\'une grille de notation et des objets associés vers une autre grille, cette dernière étant nettoyée automatiquement.'
+    args = "id_grille_origine id_grille_destination"
 
     def handle(self, *args, **options):
         if len(args) < 2:
@@ -19,34 +19,45 @@ class Command(BaseCommand):
             return
         
         id_grille = int(args[0])
-        promotion = int(args[1])
+        id_desti = int(args[1])
 
-        grille = GrilleNotation.objects.get(id=id_grille)        
-        ensembles = EnsembleCapacite.objects.filter(grille=grille)
+        grille = GrilleNotation.objects.get(id=id_grille)
+        ngrille = GrilleNotation.objects.get(id=id_desti)
+
+        # Nettoyage des objets liées à la grille de destination
+        # Les capacites et savoirs être sont supprimés en cascade
+        SavoirEtre.objects.filter(grille=ngrille).delete()
+        Theme.objects.filter(grille=ngrille).delete()
+        EnsembleCapacite.objects.filter(grille=ngrille).delete()
+
+        themes = Theme.objects.filter(grille=grille)
         savoirs = SavoirEtre.objects.filter(grille=grille)
-        
-        ngrille = copy(grille)
-        ngrille.id = None
-        ngrille.promotion = promotion
-        ngrille.save()
 
         for sv in savoirs:
             nsv = copy(sv)
             nsv.id = None
             nsv.grille = ngrille
             nsv.save()
-            
-        for ens in ensembles:
-            capacites = Capacite.objects.filter(ensemble=ens)
-            nens = copy(ens)
-            nens.id = None
-            nens.grille = ngrille
-            nens.save()
 
-            for cap in capacites:
-                ncap = copy(cap)
-                ncap.id = None
-                ncap.ensemble = nens
-                ncap.save()
+        for th in themes:
+            ensembles = EnsembleCapacite.objects.filter(theme=th)
+            nth = copy(th)
+            nth.id = None
+            nth.grille = ngrille
+            nth.save()
+            
+            for ens in ensembles:
+                capacites = Capacite.objects.filter(ensemble=ens)
+                nens = copy(ens)
+                nens.id = None
+                nens.grille = ngrille
+                nens.theme = nth
+                nens.save()
+
+                for cap in capacites:
+                    ncap = copy(cap)
+                    ncap.id = None
+                    ncap.ensemble = nens
+                    ncap.save()
                 
-        print u'Copie de la grille %s pour l\'année %d' % (grille, promotion)
+        print u'Copie de la grille %s vers la grille %s' % (grille, ngrille)
