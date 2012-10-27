800058a2076606057783a7e18ef07436a27e9e32
Philippe Roussel <philippe.roussel@octets.fr>
2012-10-27 07:21:18 +0000

    Notation : mise à jour du script de dump des grilles de notation
    git-svn-id: svn+ssh://bipbip/home/svn/octets/cfai/apprentis/trunk@2726 22393631-9731-0410-a56f-bd5f96f33d13

--- dumpgrille.sh
+++ dumpgrille.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python manage.py dumpdata --indent=2 notation.theme notation.ensemblecapacite notation.capacite > notation/fixtures/ngrilles.json
+python manage.py dumpdata --indent=2 notation.grillenotation notation.theme notation.ensemblecapacite notation.capacite notation.savoiretre > notation/fixtures/ngrilles.json
