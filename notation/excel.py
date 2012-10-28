facbfa1b892b1b7362a38af15c5b1ce48573ff68
Philippe Roussel <philippe.roussel@octets.fr>
2012-10-28 12:07:51 +0000

    Notation : suite de la mise en page du tableau excel
    git-svn-id: svn+ssh://bipbip/home/svn/octets/cfai/apprentis/trunk@2744 22393631-9731-0410-a56f-bd5f96f33d13

--- notation/excel.py
+++ notation/excel.py
@@ -23,6 +23,7 @@ def bulletin_xls(request, blt):
     sheet.set_portrait(False)
     
     # Largeur des colonnes
+    sheet.col(0).width /= 3
     sheet.col(1).width = 12000
     sheet.col(5).width = 10000
     sheet.col(6).width = 10000
@@ -37,32 +38,33 @@ def bulletin_xls(request, blt):
     sheet.write_merge(lig, lig, 1, 4, u'Filière ITII, Organisme coordinateur CFAI Sud Franche-Comté; Branche Professionnelle UIMM', gras)
     lig += 1
 
-    gros = easyxf('font: name Arial, bold on, height 240; borders: left medium, right medium')
-    normal = easyxf('font: name Arial, bold on, height 180; borders: left medium, right medium')
+    gros = easyxf('font: name Arial, bold on, height 200; borders: left medium, right medium; align: vert centre')
+    normal = easyxf('font: name Arial, bold on, height 160; borders: left medium, right medium; align: vert centre')
     sheet.write_merge(lig, lig, 1, 4, u'Apprenti : %s' % (blt.eleve.get_profile().nom_complet), gros)
     sheet.row(lig).height = sheet.row(lig).height * 3 / 2
     lig += 1
     sheet.write_merge(lig, lig, 1, 4, u'Années de formation : %s / %s' % (blt.grille.promotion, blt.grille.promotion + blt.grille.duree), normal)
     lig += 1
-    sheet.write_merge(lig, lig, 1, 4, u'N° de tél portable %s : Adresse email : %s' % (blt.eleve.get_profile().phone_number, blt.eleve.email), normal)
+    sheet.write_merge(lig, lig, 1, 4, u'N° de tél portable : %s            Adresse email : %s' % (blt.eleve.get_profile().phone_number, blt.eleve.email), normal)
     lig += 1
     
     sheet.write_merge(lig, lig, 1, 4, u'Entreprise : %s' % (blt.entreprise), gros)
     sheet.row(lig).height = sheet.row(lig).height * 3 / 2
     lig += 1
     sheet.write_merge(lig, lig, 1, 4, u'Adresse : %s' % (blt.entreprise.description), normal)
+    sheet.row(lig).height = sheet.row(lig).height * 2
     lig += 1
     
     sheet.write_merge(lig, lig, 1, 4, u'Tuteur : %s' % (blt.tuteur.get_profile().nom_complet), gros)
     sheet.row(lig).height = sheet.row(lig).height * 3 / 2
     lig += 1
-    sheet.write_merge(lig, lig, 1, 4, u'N° de tél portable : %s Adresse email : %s' % (blt.tuteur.get_profile().phone_number, blt.tuteur.email), normal)
+    sheet.write_merge(lig, lig, 1, 4, u'N° de tél portable : %s            Adresse email : %s' % (blt.tuteur.get_profile().phone_number, blt.tuteur.email), normal)
     lig += 1
-    normal = easyxf('font: name Arial, bold on, height 180; borders: left medium, right medium, bottom medium')
+    normal = easyxf('font: name Arial, bold on, height 160; borders: left medium, right medium, bottom medium')
     sheet.write_merge(lig, lig, 1, 4, u'Chargé de promotion : %s' % (blt.formateur.get_profile().nom_complet), normal)
     
-    titre = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre')
-    titrev = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre; pattern: pattern solid, fore-colour green')
+    titre = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre')
+    titrev = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre; pattern: pattern solid, fore-colour green')
     lig = 13
     sheet.row(lig).height = sheet.row(lig).height * 5 / 2
     sheet.write(lig, 1, u'Capacités professionnelles et Tâches professionnelles\r\n(Être capable de...)', titrev)
@@ -73,15 +75,15 @@ def bulletin_xls(request, blt):
     sheet.write(lig, 6, u'Résultats - Indicateurs de performance - Validation\r\n(Action réalisées ou initiées en entreprise)\r\n2ème année', titre)
     sheet.write(lig, 7, u'Résultats - Indicateurs de performance - Validation\r\n(Action réalisées ou initiées en entreprise)\r\n3ème année', titre)
 
-    titre = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz centre; pattern: pattern solid, fore-colour grey25')
-    titreg = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour grey25')
-    normal = easyxf('font: name Arial, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre, wrap true')
-    commentaire = easyxf('font: name Arial, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert top, wrap true')
-    centre = easyxf('font: name Arial, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz centre')
-    note = easyxf('font: name Arial, height 160, bold on; align: vert centre')
-    notec = easyxf('font: name Arial, height 160, bold on; align: vert centre, horiz centre')
-    vertical = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre, rotation 90')
-    grasdroite = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz right')
+    titre = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz centre; pattern: pattern solid, fore-colour grey25')
+    titreg = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour grey25')
+    normal = easyxf('font: name Arial, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre, wrap true')
+    commentaire = easyxf('font: name Arial, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert top, wrap true')
+    centre = easyxf('font: name Arial, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz centre, wrap true')
+    note = easyxf('font: name Arial, height 140, bold on; align: vert centre')
+    notec = easyxf('font: name Arial, height 140, bold on; align: vert centre, horiz centre')
+    vertical = easyxf('font: name Arial, bold on, height 180; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre, rotation 90')
+    grasdroite = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz right')
 
     lig += 1
     
@@ -100,7 +102,6 @@ def bulletin_xls(request, blt):
                 sheet.write(lig, 4, evaluation(notes.filter(annee=2)), centre)
                 lig += 1
 
-            
             comms = Commentaire.objects.filter(bulletin=blt, ensemble=ens)
             for year in [0, 1, 2]:
                 comm = comms.filter(annee=year)
@@ -124,9 +125,9 @@ def bulletin_xls(request, blt):
 
     # Fin de tableau avec les moyennes et les savoirs etre
     lig += 1
-    titreg = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour sea_green')
-    sheet.write(lig, 1, u'Note globale "compétence" sur 20 (sera entre 4 et 20)', titreg)
-    gras = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz centre;')
+    titreg = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour sea_green')
+    sheet.write(lig, 1, u'Note globale "compétence" sur 20', titreg)
+    gras = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre, horiz centre;')
 
 
     moyennes = blt.moyenne_set.all()
@@ -135,8 +136,8 @@ def bulletin_xls(request, blt):
         sheet.write(lig, 2 + year, moyenne and moyenne[0].valeur_cp or 0, gras)
 
     lig += 2
-    titre = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre; pattern: pattern solid, fore-colour pink')
-    normal = easyxf('font: name Arial, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre, wrap true;')
+    titre = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: horiz centre, vert centre; pattern: pattern solid, fore-colour pink')
+    normal = easyxf('font: name Arial, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre, wrap true;')
     sheet.write_merge(lig, lig, 1, 4, u'Savoir être (notes de 0 à 5)', titre)
 
     lig += 1
@@ -154,17 +155,17 @@ def bulletin_xls(request, blt):
         sheet.write(lig, 4, n and n[0].valeur or '', centre)
         lig += 1
 
-    normal = easyxf('font: name Arial, bold on, height 180; borders: left medium, right medium, bottom medium, top medium; align: vert top, wrap true')
+    normal = easyxf('font: name Arial, bold on, height 160; borders: left medium, right medium, bottom medium, top medium; align: vert top, wrap true')
     sheet.write_merge(start, lig - 1, 5, 6, u'Commentaires généraux :\r\n%s' % blt.commentaires_generaux, normal)
 
-    titreg = easyxf('font: name Arial, bold on, height 160; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour light_blue')
+    titreg = easyxf('font: name Arial, bold on, height 140; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour pink')
     sheet.write(lig, 1, u'Moyenne "savoir être" (sur 20)', titreg)
     for year in [0, 1, 2]:
         moyenne = moyennes.filter(annee=year)
         sheet.write(lig, 2 + year, moyenne and moyenne[0].valeur_sv or 0, gras)
         
     lig += 2
-    titreg = easyxf('font: name Arial, bold on, height 200; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour light_blue')
+    titreg = easyxf('font: name Arial, bold on, height 180; borders: left medium, top medium, right medium, bottom medium; align: vert centre; pattern: pattern solid, fore-colour light_blue')
     sheet.row(lig).height = sheet.row(lig).height * 3 / 2
     sheet.write(lig, 1, u'Note entreprise (sur 20)', titreg)
     for year in [0, 1, 2]:
