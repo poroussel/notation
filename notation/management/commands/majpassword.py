1e525e2469dd3d455208be02678b74ccaf0e9bc8
Philippe Roussel <philippe.roussel@octets.fr>
2013-03-29 15:01:02 +0000

    Notation : mise à jour de la commande de reset des mots de passe
    git-svn-id: svn+ssh://bipbip/home/svn/octets/cfai/apprentis/trunk@2977 22393631-9731-0410-a56f-bd5f96f33d13

--- notation/management/commands/majpassword.py
+++ notation/management/commands/majpassword.py
@@ -1,21 +1,28 @@
 # -*- coding: utf-8 -*-
 
+import time
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
+from django.template.loader import render_to_string
+from django.core.mail import send_mail
 from django.conf import settings
 from cfai.notation.models import *
 
 class Command(BaseCommand):
-    help = u'Réinitialisation des mots de passe des tuteurs et des chargés de promotion et envoi de nouveaux emails'
+    help = u'Réinitialisation des mots de passe des tuteurs et envoi de nouveaux emails'
 
     def handle(self, *args, **options):
         users = User.objects.all()
         current_site = Site.objects.get_current()
         for u in users:
             profil = u.get_profile()
-            if profil.is_tuteur() or profil.is_formateur():
-                u.set_password(u.username)
+            if profil.is_tuteur():
+                password = User.objects.make_random_password()
+                u.set_password(password)
                 u.save()
                 if len(u.email) > 0:
                     body = render_to_string('maj_compte.txt', {'profil' : profil, 'site' : current_site})
                     send_mail(u'[CFAI/ENSMM] Mise à jour de votre compte', body, settings.SERVER_EMAIL, [u.email], fail_silently=True)
+                    time.sleep(5)
+
