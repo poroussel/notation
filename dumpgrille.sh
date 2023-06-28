38fdb441f762905094b05fda2faf66e9da6820f8
Philippe Roussel <p.o.roussel@free.fr>
2023-06-29 00:26:04 +0200

    Suppression de vieux scripts

--- dumpgrille.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python manage.py dumpdata --indent=2 notation.grillenotation notation.theme notation.ensemblecapacite notation.capacite notation.savoiretre > notation/fixtures/ngrilles.json
